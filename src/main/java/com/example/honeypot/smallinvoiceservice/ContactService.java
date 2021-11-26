b7358f52f77799a5b05b7ab93be7a3ae1cde6e7d
Ruven Schuerch <schueruv@hotmail.com>
2021-11-26 10:45:23 +0100

    HoneyPot-2: End-Point getContactById Server side added

--- src/main/java/com/example/honeypot/smallinvoiceservice/ContactService.java
+++ src/main/java/com/example/honeypot/smallinvoiceservice/ContactService.java
@@ -2,14 +2,16 @@ package com.example.honeypot.smallinvoiceservice;
 
 import com.example.honeypot.model.Contact;
 import com.example.honeypot.utils.FieldMapper;
-import com.example.smallinvoicespringfeign.model.ContactGET;
+import com.example.smallinvoicespringfeign.model.*;
 import com.example.smallinvoicespringfeign.service.SmallInvoiceApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ContactService {
@@ -21,10 +23,19 @@ public class ContactService {
         List<ContactGET> contactGETS =  apiService.getContacts(null, null);
 
         for (ContactGET contact: contactGETS) {
+            // TODO: 26.11.2021 REPLACE SYSTEM.OUT.PRINTLN WITH ACTUAL LOGGER
             System.out.println("Contact received: " + contact);
             contacts.add(FieldMapper.mapFromJson(FieldMapper.mapToJson(contact), Contact.class));
         }
         return contacts;
     }
+
+    public Contact getContactById(int contactId) throws IOException {
+        Contact contact = FieldMapper.mapFromJson(FieldMapper.mapToJson(apiService.getContactById(contactId, null)), Contact.class);
+        // TODO: 26.11.2021 REPLACE SYSTEM.OUT.PRINTLN WITH ACTUAL LOGGER
+        System.out.println("Contact received: " + contact);
+        return contact;
+    }
+
 
 }
--- src/main/java/com/example/honeypot/web/rest/ContactRestController.java
+++ src/main/java/com/example/honeypot/web/rest/ContactRestController.java
@@ -3,10 +3,9 @@ package com.example.honeypot.web.rest;
 import com.example.honeypot.model.Contact;
 import com.example.honeypot.smallinvoiceservice.ContactService;
 import com.example.honeypot.web.exception.InternalServerError;
+import com.example.honeypot.web.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,4 +29,36 @@ public class ContactRestController{
             throw new InternalServerError("Internal Error at getContacts");
         }
     }
+
+    @GetMapping("/contacts/{contactId}")
+    public Contact getContactById(@PathVariable Integer contactId){
+        try {
+            // TODO: 26.11.2021 REPLACE SYSTEM.OUT.PRINTLN WITH ACTUAL LOGGER
+            System.out.println("received API call for getContactByID with ID: " + contactId);
+            Contact contact = contactService.getContactById(contactId);
+            if (contact == null)
+                throw new ResourceNotFoundException("Authenticated remote user not found");
+            return contact;
+
+        } catch (Exception ex){
+            // TODO: 26.11.2021 REPLACE SYSTEM.OUT.PRINTLN WITH ACTUAL LOGGER
+            System.out.println("getContactById Error: " + ex.getMessage());
+            throw new InternalServerError("Internal Error at getContactById");
+        }
+    }
+/*
+    @PutMapping("contacts/{contactId}")
+    public Contact updateContact(@RequestBody Contact contact, @PathVariable Integer contactId){
+        if (contact.getId() != contactId)
+            throw new InternalServerError("Internal Error at updateContact: ID Missmatch");
+
+        try {
+            // TODO: 26.11.2021 REPLACE SYSTEM.OUT.PRINTLN WITH ACTUAL LOGGER
+            System.out.println("received API call for updateContact with ID: " + contactId);
+
+
+    }
+*/
+    //@PostMapping("/contacts")
+
 }
